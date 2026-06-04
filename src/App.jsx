c434f37fae46f277b949c82d534374dc44e2e109
njonnavada <neelimajonnavada@gmail.com>
2026-06-05 01:01:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+// App.jsx
+function App() {
+  return <h1>Hello Neelima!</h1>;
+}
+
+export default App;
